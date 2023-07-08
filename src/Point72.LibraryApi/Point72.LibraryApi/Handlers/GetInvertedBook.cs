eddd5f3547180084d573d375c9b65d194a9d8e60
skoncewicz <sylwester.koncewicz@gmail.com>
2023-07-08 10:02:18 +0200

    Initial Commit - .gitignore + sln stub

---
+++ src/Point72.LibraryApi/Point72.LibraryApi/Handlers/GetInvertedBook.cs
@@ -0,0 +1,9 @@
+﻿namespace Point72.LibraryApi.Handlers;
+
+// 2.	Endpoint to invert words in Title of the given book (/api/invertwords).
+// Book is identified by ID. When called, service must invert all words in the Title of the book (words are sequences of
+// characters and numbers separated by spaces or other signs like commas, semicolons etc.) 
+// Returns: book object
+public class GetInvertedBook
+{
+}
---
+++ src/Point72.LibraryApi/Point72.LibraryApi/Handlers/GetReport.cs
@@ -0,0 +1,9 @@
+﻿namespace Point72.LibraryApi.Handlers;
+
+// 3.	Endpoint to generate report (/api/report) – list of users with total count of books taken and total count of
+// days this user holds all the books.
+// Returns: User details, Total books he/she is holding, Total days he/she holds them.
+public class GetReport
+{
+    
+}
